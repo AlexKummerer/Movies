911587c3b6bf9794e8fcd0bfbe52a52841a6165f
alexkummerer <alexander@thomas-kummerer.de>
2024-06-20 13:47:56 +0200

    update, add, delete movies menu

--- main.py
+++ main.py
@@ -1,14 +1,76 @@
-# This is a sample Python script.
+import movie_storage
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def list_movies():
+    movies = movie_storage.list_movies()
+    print(f"\n{len(movies)} movies in total\n")
+    for movie in movies:
+        print(f"{movie['Title']} ({movie['Year']}): {movie['Rating']}")
+
+
+def add_movie():
+    title = input("Enter new movie name: ")
+    year = int(input("Enter new movie year: "))
+    rating = float(input("Enter new movie rating: "))
+
+    movie_storage.add_movie(title, year, rating)
+
+
+def delete_movie():
+    title = input("\nEnter movie name to delete: ")
+    movie_storage.delete_movie(title)
+
+
+def update_movie():
+    movie_name = input("\nEnter movie name: ")
+    movie = movie_storage.get_movie_by_title(movie_name)
+
+    if movie is None:
+        print(f"Movie {movie_name} doesn't exist")
+    else:
+        new_rating = float(input("Enter new movie rating: "))
+        movie_storage.update_movie(movie_name, new_rating)
+
+
+menu_options = {
+    "0": ("Exit", None),
+    "1": ("List movies", list_movies),
+    "2": ("Add movie", add_movie),
+    "3": ("Delete movie", delete_movie),
+    "4": ("Update movie", update_movie),
+}
+
+
+def display_menu():
+    """Display the application menu"""
+    print("\n******* My Movies Database *******")
+    print("Menu:")
+    for key, (descr, _) in menu_options.items():
+        print(f"{key}. {descr}")
 
 
 def main():
-    pass
+    """Main function to run the application"""
+    while True:
+        display_menu()
+        choice = input(f"Enter choice (0-{len(menu_options) -1 }): ").strip()
+        if choice in menu_options:
+            descr, function = menu_options[choice]
+            if choice == "0":
+                print("\nExiting the application, Goodbye!")
+                break
+            else:
+                print(f"\nExecuting: {descr}")
+                function()
+        else:
+            print(
+                f"\nInvalid choice. Please enter a number between 0 and {len(menu_options) -1 }."
+            )
+
+        input("\nPress Enter to return to the menu...")
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- movie_storage.py
+++ movie_storage.py
@@ -1,44 +1,21 @@
 import json
 import os
 
+FILE_NAME: str = "movies.json"
 
-def list_movies(filename="movies.json"):
+
+def list_movies() -> list[dict]:
     """
-    Returns a dictionary of dictionaries that
-    contains the movies information in the database.
-
     The function loads the information from the JSON
     file and returns the data.
 
-    For example, the function may return:
-    {
-      "Titanic": {
-        "rating": 9,
-        "year": 1999
-      },
-      "..." {
-        ...
-      },
-    }
-    or
-    {
-        "Title": "Inception",
-        "Rating": 8.8,
-        "Year": 2010,
-        "Director": "Christopher Nolan",
-        "Genre": "Science Fiction"
-    },
-    {
-        "Title": "The Matrix",
-        "Rating": 8.7,
-        "Year": 1999,
-        "Director": "Lana Wachowski, Lilly Wachowski",
-        "Genre": "Action"
-    },
+    Returns:
+        list[dict]: Returns a dictionary of dictionaries that
+                    contains the movies information in the database.
     """
 
-    if not os.path.exists(filename):
-        print(f"No file named {filename} found.")
+    if not os.path.exists(FILE_NAME):
+        print(f"No file named {FILE_NAME} found.")
         return []
 
     with open("movies.json", "r") as file:
@@ -46,17 +23,29 @@ def list_movies(filename="movies.json"):
         if not movies:
             print("no movies found in the file.")
             return []
-        print(f"{len(movies)} movies in total")
-        for movie in movies:
-           print(f"{movie['Title']} ({movie['Year']}): {movie['Rating']}")  
         return movies
 
 
-def add_movie(title, year, rating):
+def save_movies(movies: list[dict]):
+    """Save movies to the JSON file
+
+    Args:
+        movies (list[dict]): movies list to save
     """
-    Adds a movie to the movies database.
+
+    with open(FILE_NAME, "w") as file:
+        json.dump(movies, file, indent=4)
+
+
+def add_movie(title: str, year: int, rating: float) -> None:
+    """Adds a movie to the movies database.
     Loads the information from the JSON file, add the movie,
     and saves it. The function doesn't need to validate the input.
+
+    Args:
+        title (str): title of the movie
+        year (int): year of the movie
+        rating (float): rating of the movie
     """
     current_movies: list[dict] = list_movies()
     if not current_movies:
@@ -64,26 +53,64 @@ def add_movie(title, year, rating):
     new_movie: dict = {"Title": title, "Year": year, "Rating": rating}
     current_movies.append(new_movie)
 
-    movies_json = json.dumps(current_movies, indent=4)
+    try:
+        save_movies(current_movies)
+        print(f"Movie {title} succesfully added")
+    except Exception as e:
+        print(f"There was an error by adding your movie {title}")
+        print("Error Message:", e)
 
-    with open("movies.json", "w") as file:
-        file.write(movies_json)
 
-
-def delete_movie(title):
+def delete_movie(title: str) -> None:
     """
     Deletes a movie from the movies database.
     Loads the information from the JSON file, deletes the movie,
     and saves it. The function doesn't need to validate the input.
     """
-    pass
+    movies = list_movies()
+
+    updated_movies = [
+        movie for movie in movies if movie["Title"].lower() != title.lower()
+    ]
+    save_movies(updated_movies)
+
+    print(
+        f"\nMovie '{title}' deleted."
+        if len(movies) != len(updated_movies)
+        else f"\nNo movie found with the title '{title}'."
+    )
 
 
-def update_movie(title, rating):
+def get_movie_by_title(title: str) -> dict:
+    """get the movie by title
+
+    Args:
+        title (str): Title of the movie
+
+    Returns:
+        dict: get the information of the film
     """
-    Updates a movie from the movies database.
-    Loads the information from the JSON file, updates the movie,
-    and saves it. The function doesn't need to validate the input.
-    """
-    pass
+    movies = list_movies()
 
+    movie = next(
+        (movie for movie in movies if movie["Title"].lower() == title.lower()), None
+    )
+
+    return movie
+
+
+def update_movie(title: str, rating: float) -> None:
+    """Updates a movie from the movies database
+
+    Args:
+        title (str): title of the movie
+        rating (float): new rating of the movie
+    """
+
+    movies = list_movies()
+    for movie in movies:
+        if movie["Title"].lower() == title.lower():
+            movie["Rating"] = rating
+            break
+    save_movies(movies)
+        
